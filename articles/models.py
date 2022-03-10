e461c6becc8703810011d05cab2b814caef9ae3e
Sozinova Dayana <daanasozinova6@gmail.com>
2022-03-10 17:25:39 +0300

    git init

---
+++ articles/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение', )
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
+
+    def __str__(self):
+        return self.title
+
+
+class Object(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Тематики статьи')
+    relationshipss = models.ManyToManyField(Article, through='Relationship')
+
+    class Meta:
+        verbose_name = 'Тематики статьи'
+        verbose_name_plural = 'Тематики статьи'
+
+
+    def __str__(self):
+        return self.name
+
+
+class Relationship(models.Model):
+    objects = models.ForeignKey(Object, on_delete=models.CASCADE, related_name='relationships')
+    articles = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='relationships')
+
+
